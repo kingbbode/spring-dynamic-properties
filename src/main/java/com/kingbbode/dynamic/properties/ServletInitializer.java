242d53a582bd19e2092de19ebbfd7cfbdaf24714
kingbbode <kingbbode@zuminternet.com>
2016-09-29 18:27:24 +0900

    최초커밋

---
+++ src/main/java/com/kingbbode/dynamic/properties/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.kingbbode.dynamic.properties;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringDynamicPropertiesApplication.class);
+	}
+
+}
---
+++ src/main/java/com/kingbbode/dynamic/properties/SpringDynamicPropertiesApplication.java
@@ -0,0 +1,12 @@
+package com.kingbbode.dynamic.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDynamicPropertiesApplication.class, args);
+	}
+}
