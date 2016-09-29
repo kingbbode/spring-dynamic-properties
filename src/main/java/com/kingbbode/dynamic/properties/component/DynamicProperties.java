8a2ca4d4d31df28f34ee33987659a0997df9923f
권용근 <kingbbode@gmail.com>
2016-09-29 23:49:41 +0900

    code clean

--- src/main/java/com/kingbbode/dynamic/properties/component/DynamicProperties.java
+++ src/main/java/com/kingbbode/dynamic/properties/component/DynamicProperties.java
@@ -3,45 +3,34 @@ package com.kingbbode.dynamic.properties.component;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by YG on 2016-09-28.
  */
 @Component
 public class DynamicProperties {
-    private static final Logger logger = LoggerFactory.getLogger(DynamicProperties.class);
-    
     private PropertiesConfiguration configuration;
     private FileChangedReloadingStrategy strategy;
     
-    private static final String DEV = "admin.dev";
-    private static final String LOGIN = "admin.login";
-    private static final int NAME = 0;
-    private static final int EMAIL = 1;
+    private static final String SERVER_FILE = "file:/data/dynamic.properties";
+    private static final String CLASSPATH_FILE = "dynamic.properties";
     
     @PostConstruct
     private void init() {
         try {
-            configuration = new PropertiesConfiguration("file:/data/dynamic.properties");            
+            configuration = new PropertiesConfiguration(SERVER_FILE);
         } catch (ConfigurationException e) {
             try {
-                configuration = new PropertiesConfiguration("dynamic.properties");
+                configuration = new PropertiesConfiguration(CLASSPATH_FILE);
             } catch (ConfigurationException e1) {
                 configuration = null;
             }
         }
         if(configuration != null){
             strategy = new FileChangedReloadingStrategy();
-            strategy.setRefreshDelay(1000*60*60*24*30);
             configuration.setReloadingStrategy(strategy);
         }
     }
